ac46f50f2740a9ed37a62586af9cd87c2834cd8d
CJCniubi666 <140289246+CJCniubi666@users.noreply.github.com>
2024-01-12 10:55:39 +0000

    Deploying to gh-pages from @ CJCniubi666/cjcniubi666.github.io@1d252758a0ae1b1e6d64fe2b5bbf7746bc4c9dfe 🚀

--- assets/提升为完全交互式的shell.html-9fd6e22d.js
+++
@@ -1,8 +0,0 @@
-import{_ as s}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as a,e}from"./app-916f4302.js";const t={},l=e(`<div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ python <span class="token parameter variable">-c</span> <span class="token string">&#39;import pty; pty.spawn(&quot;/bin/bash&quot;)&#39;</span>
-Ctrl-Z
-$ stty raw <span class="token parameter variable">-echo</span>
-$ <span class="token function">fg</span>
-$ reset
-$ <span class="token builtin class-name">export</span> <span class="token assign-left variable"><span class="token environment constant">SHELL</span></span><span class="token operator">=</span>bash
-//$ <span class="token builtin class-name">export</span> <span class="token assign-left variable"><span class="token environment constant">TERM</span></span><span class="token operator">=</span>xterm-256color
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),i=[l];function o(r,c){return n(),a("div",null,i)}const m=s(t,[["render",o],["__file","提升为完全交互式的shell.html.vue"]]);export{m as default};
